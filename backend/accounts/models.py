5f93e3da348e8af9fa3c5c91eeb47ce9f8224067
Anim Malvat <malvatanim@gmail.com>
2025-07-29 16:36:12 -0400

    Add CustomUser model with role field and add AUTH USER MODEL in settings

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('instructor', 'Instructor'), 
+        ("admin", "Admin"),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
